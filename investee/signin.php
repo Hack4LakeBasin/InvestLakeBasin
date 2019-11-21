51a506b5e3edea8530b1a8500969230d2ec22054
kanji-karanja <karimkanji101@gmail.com>
2019-11-21 23:35:07 +0300

    Modified navbar  and file directories

--- investee/signin.php
+++ investee/signin.php
@@ -4,36 +4,41 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>InvestLakeBasin</title>
-    <meta name="theme-color" content="rgb(35,198,208)">
-    <meta name="twitter:description" content="Invest Lake Basin is a platform to link invetees to investors.">
-    <meta name="twitter:image" content="assets/img/invest_lakebasin.png">
-    <meta name="description" content="Invest Lake Basin is a platform to link invetees to investors.">
-    <meta name="" content="">
-    <meta property="og:image" content="assets/img/invest_lakebasin.png">
+    <title>Home - InvestLakeBasin</title>
+    <meta name="twitter:description" content="InvestLakeBasin is a platform for connecting Small Scale Farmers as Investees to investors based on their portfolio created.">
     <meta name="twitter:title" content="InvestLakeBasin">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:image" content="assets/img/invest_lakebasin.png">
+    <meta name="description" content="InvestLakeBasin is a platform for connecting Investees to investors based on their portfolio created.">
+    <meta property="og:type" content="website">
+    <meta name="twitter:image" content="assets/img/invest_lakebasin.png">
     <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
     <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
     <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
     <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
-    <link rel="icon" type="image/png" sizes="200x200" href="assets/img/invest_lakebasin.png">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="manifest" href="manifest.json">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
 </head>
 
 <body>
-    <nav class="navbar navbar-light navbar-expand-md fixed-top bg-dark" style="background-color: #070707;color: rgb(230,233,237);">
-        <div class="container-fluid"><a class="navbar-brand" href="#" style="color: rgba(255,255,255,0.9);">InvestLakeBasin</a>
-            <div class="collapse navbar-collapse" id="navcol-1">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item" role="presentation"><a class="nav-link active text-white" href="#">First Item</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link text-white" href="#">Second Item</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link text-white" href="#">Third Item</a></li>
+<nav class="navbar navbar-dark navbar-expand-md fixed-top bg-white portfolio-navbar gradient">
+        <div class="container"><a class="navbar-brand logo" href="#">InvestLakeBasin</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navbarNav">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="../index.html">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="../index.html#how-it-works">About&nbsp;</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="javascript:void(0)">Get funding</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="../investors">Start Investing</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <div class="container" style="margin-top: 80px;">
+    <div class="container" style="margin-top: 100px;">
         <h1 class="text-center">Sign in</h1>
         <form><label>Email, Phone Number or Username:</label><input class="form-control" type="text" placeholder="Email, Phone Number or Username"><label style="margin-top: 10px;">Password:</label><input class="form-control" type="text" placeholder="Password">
             <button
