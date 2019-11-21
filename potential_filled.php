e2a37dedf383d7ab01474b40b25d677f14fbd260
kanji-karanja <karimkanji101@gmail.com>
2019-11-21 21:19:23 +0300

    Changed all html to php extensions

---
+++ potential_filled.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>InvestLakeBasin</title>
+    <meta name="theme-color" content="rgb(35,198,208)">
+    <meta name="twitter:description" content="Invest Lake Basin is a platform to link invetees to investors.">
+    <meta name="twitter:image" content="assets/img/invest_lakebasin.png">
+    <meta name="description" content="Invest Lake Basin is a platform to link invetees to investors.">
+    <meta name="" content="">
+    <meta property="og:image" content="assets/img/invest_lakebasin.png">
+    <meta name="twitter:title" content="InvestLakeBasin">
+    <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
+    <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
+    <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
+    <link rel="icon" type="image/png" sizes="89x100" href="assets/img/invest_lakebasin_favicon.png">
+    <link rel="icon" type="image/png" sizes="200x200" href="assets/img/invest_lakebasin.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="manifest" href="manifest.json">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top bg-dark" style="background-color: #070707;color: rgb(230,233,237);">
+        <div class="container-fluid"><a class="navbar-brand" href="#" style="color: rgba(255,255,255,0.9);">InvestLakeBasin</a>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" role="presentation"><a class="nav-link active text-white" href="#">First Item</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-white" href="#">Second Item</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link text-white" href="#">Third Item</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container" style="margin-top: 90px;">
+        <h2 class="text-center">Potential Investors</h2>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Lavender Aluju</h4>
+            <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
+            <p class="card-text">This is what the investor will offer you. You can choose to decline this or accept it as it is.</p><a class="card-link" href="#">Accept</a><a class="card-link" href="#">Decline</a></div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">Joseph Njogu</h4>
+            <h6 class="text-muted card-subtitle mb-2">Subtitle</h6>
+            <p class="card-text">This is what the investor will offer you. You can choose to decline this or accept it as it is.</p><a class="card-link" href="#">Accept</a><a class="card-link" href="#">Decline</a></div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
